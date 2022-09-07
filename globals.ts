d23037dcb09651da372c0c1084b941d71eb72db9
Lenny Reitz <lennyreitz28@gmail.com>
2022-09-07 15:24:21 +0200

    refactor: project structure

--- globals.ts
+++
@@ -1,4 +0,0 @@
-export const SCOPES = 'https://www.googleapis.com/auth/calendar'
-export const GOOGLE_PRIVATE_KEY = './calsync-private-key.pem'
-export const CALENDAR_CACHE_FILE = './calendarCache.json'
-export const CONFIG_FILE = './config.yml'
---
+++ src/globals.ts
@@ -0,0 +1,6 @@
+const CONFIG_PATH = '../config'
+
+export const SCOPES = 'https://www.googleapis.com/auth/calendar'
+export const GOOGLE_PRIVATE_KEY = `${CONFIG_PATH}/calsync-private-key.pem`
+export const CALENDAR_CACHE_FILE = `${CONFIG_PATH}/calendarCache.json`
+export const CONFIG_FILE = `${CONFIG_PATH}/config.yml`
