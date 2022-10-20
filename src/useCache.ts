067f88c379f2868bfcb84e2b6e12a4d4665b1acb
Lenny Reitz <lennyreitz28@gmail.com>
2022-10-20 11:14:44 +0200

    feat: create cache file if doesn't exist

--- src/useCache.ts
+++ src/useCache.ts
@@ -4,6 +4,10 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export const useCache = () => {
+  if (!fs.existsSync(process.env.CALENDAR_CACHE_PATH)) {
+    fs.writeFileSync(process.env.CALENDAR_CACHE_PATH, '{}')
+  }
+
   let cache = JSON.parse(fs.readFileSync(process.env.CALENDAR_CACHE_PATH, 'utf8'))
 
   const loadCache = () => {
