7ff3e89c561fb42c484efbe5779e062a45d837e9
Lenny Reitz <lennyreitz28@gmail.com>
2022-10-20 10:35:35 +0200

    chore: improve eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,24 @@
 module.exports = {
   root: true,
-  parser: "@typescript-eslint/parser",
-  plugins: [
-    "@typescript-eslint"
-  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended"
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:unicorn/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:prettier/recommended',
   ],
   rules: {
     'vue/multi-word-component-names': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    'no-console': process.env.NODE_ENV === 'production' ? ['warn',
-      { allow: ['debug'
-        ]
-      }
-    ] : 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? ['warn', { allow: ['debug'] }] : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-undef': 'off',
     '@typescript-eslint/no-extra-semi': 'off', // collides with prettier
-    '@typescript-eslint/no-unused-vars': ['error',
-      { destructuredArrayIgnorePattern: '^_'
-      }
-    ], // useful for map() when returning same format
+    '@typescript-eslint/no-unused-vars': ['error', { destructuredArrayIgnorePattern: '^_' }], // useful for map() when returning same format
 
     'unicorn/filename-case': 'off',
     'unicorn/prevent-abbreviations': 'off',
@@ -32,8 +26,7 @@ module.exports = {
     'import/order': [
       'warn',
       {
-        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'object', 'type'
-        ],
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'object', 'type'],
         pathGroups: [
           {
             pattern: '@ /**',
@@ -44,8 +37,8 @@ module.exports = {
         alphabetize: {
           order: 'asc' /* sort in ascending order. Options: ['ignore', 'asc', 'desc'] */,
           caseInsensitive: false,
-          },
         },
-      ],
-    },
-  }
+      },
+    ],
+  },
+}
